085d4a301a37085477d08841f56d847afba37464
GodfreySam <godfreysam09@gmail.com>
2021-10-11 18:19:36 +0100

    first commit of the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React, { useEffect, useState } from 'react';
+import Intro from './components/into/Intro';
+import Toggle from './components/toggle/Toggle';
+
+// CONTEXT API APP
+
+const App = () => {
+  return (
+    <div>
+      <Toggle />
+      <Intro />
+    </div>
+  )
+}
+
+export default App
